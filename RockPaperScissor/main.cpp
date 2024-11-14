cf4f76416c17d7c72f97cff4717fa23be150762a
Huzaifah Tahir <tahirhuzaifah@gmail.com>
2024-11-14 01:42:41 -0500

    Changed win conditions to use maps

--- RockPaperScissor/main.cpp
+++ RockPaperScissor/main.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <iomanip>
 #include <random>
+#include <unordered_map>
 
 struct Player
 {
@@ -16,48 +17,30 @@ struct Player
     int score = 0;
 };
 
-// If else statement solution
 void choiceCheck(Player& _player, Player& _cpu)
 {
+    std::unordered_map<int, int> winConditions = {
+        {1,3}, // Rock beat scissors
+        {2,1}, // Paper beats rock
+        {3,2} // Scissor beats paper
+    };
+    
     if (_player.choice == _cpu.choice)
     {
         std::cout << "It's a draw!\n";
     }
-    else if(_player.choice == 1 && _cpu.choice == 3)
+    else if (winConditions[_player.choice] == _cpu.choice)
     {
         std::cout << "It's a win for the Player!\n" << std::endl;
         _player.score++;
-    }
-    else if(_player.choice == 1 && _cpu.choice == 2)
-    {
+    } else {
         std::cout << "It's a win for the CPU!\n" << std::endl;
         _cpu.score++;
     }
-    else if(_player.choice == 2 && _cpu.choice == 1)
-    {
-        std::cout << "It's a win for the Player\n" << std::endl;
-        _player.score++;
-    }
-    else if(_player.choice == 2 && _cpu.choice == 3)
-    {
-        std::cout << "It's a win for the CPU!\n" << std::endl;
-        _cpu.score++;
-    }
-    else if(_player.choice == 3 && _cpu.choice == 2)
-    {
-        std::cout << "It's a win for the Player\n" << std::endl;
-        _player.score++;
-    }
-    else if(_player.choice == 3 && _cpu.choice == 1)
-    {
-        std::cout << "It's a win for the CPU!\n" << std::endl;
-        _cpu.score++;
-    }
-    else
-    {
-    }
-    
-};
+}
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -87,15 +70,15 @@ int main(int argc, const char * argv[]) {
     std::cout << "You had a score of " << player.score << " & CPU had a score of " << CPU.score << ".";
     if (player.score > CPU.score)
     {
-        std::cout << " Player wins!";
+        std::cout << " Player wins!\n";
     }
     else if (player.score < CPU.score)
     {
-        std::cout << " CPU wins!";
+        std::cout << " CPU wins!\n";
     }
     else
     {
-        std::cout << " It's a draw";
+        std::cout << " It's a draw\n";
     }
     
     return 0;
